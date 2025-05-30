a92e1f5ed1d13636219bf47143a7e7ab87cebf49
elisa-os <elisa2201@gmail.com>
2025-05-30 15:35:55 -0300

    adicionei as funções create e delete

--- src/bst.cpp
+++ src/bst.cpp
@@ -1,1 +1,31 @@
 // Implementação da Árvore Binária de Busca
+
+#include <iostream> //import do python
+#include <vector>
+#include <string>
+#include <bst.h>
+
+namespace bst{
+    BinaryTree* create() {
+        BinaryTree* tree = new BinaryTree{};
+        tree->root = nullptr;
+        tree->NIL = nullptr;
+
+        return tree;
+    };
+
+    void deleteNode(Node* node) {
+        if (node == nullptr) return;
+        deleteNode(node->left);
+        deleteNode(node->right);
+
+        delete node;
+    }
+
+    void deleteTree(BinaryTree* tree) {
+        if (tree == nullptr) return;
+
+        deleteNode(tree->root);
+        tree->root = nullptr;
+    }
+}
--- src/bst.h
+++ src/bst.h
@@ -1,1 +1,41 @@
 // Implementação da Árvore Binária de Busca
+#ifndef BST_H
+#define BST_H
+
+namespace bst{
+    struct Node {
+        std::string word;
+        std::vector<int> documentIds;
+        Node* parent;
+        Node* left;
+        Node* right;
+        int height;    // usado na AVL
+        int isRed;     // usado na RBT
+    };
+
+    struct BinaryTree {
+        Node* root;
+        Node* NIL;  // usado na RBT (Opcional)
+    };
+
+      /**
+      * @brief Cria uma nova árvore.
+      * @return Uma nova árvore vazia.
+      */
+     BinaryTree* create() ;
+
+    /**
+    * @brief Função auxiliar para deletar a árvore, é chamada recursivamente para eliminar cada nó.
+    * @param node É o nó a ser deletado.
+    */
+   void deleteNode(Node* node);
+
+    /**
+    * @brief Deleta toda a árvore e libera a memória.
+    * @param tree É a árvore a ser deletada.
+    */
+     void deleteTree(BinaryTree* tree);
+
+}
+
+#endif
