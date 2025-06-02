d002efd2d9f558c21c4ef4eae6d63e366ae8b913
elisa-os <elisa2201@gmail.com>
2025-06-02 20:07:18 -0300

    Fiz a leitura dos documentos na main

--- src/main_bst.cpp
+++ src/main_bst.cpp
@@ -1,5 +1,6 @@
 // Interface via linha de comando (indexação e busca) da Árvore Binária de Busca
 #include "bst.h"
+#include "data.h"
 
 using namespace BST;
 
@@ -14,17 +15,26 @@ int main(int argc, char* argv[]) {
     std::string diretorio = argv[3];
 
     BinaryTree* tree = create(); // criar a arvore
-    InsertResult insert(BinaryTree* tree, const std::string& word, int documentId); // inserir os n_docs documentos do diretorio
+
+    vector<doc> documentos = read_documents(diretorio, n_docs);
+    for (int i = 0; i < n_docs; ++i) {
+        doc document_i = documentos[i];
+        for(size_t p = 0; p < document_i.words.size(); ++p){
+            string word = document_i.words[p];
+            int id = document_i.id;
+            insert(tree, word, id);   // inserir os n_docs documentos do diretorio
+        }
+    }
 
     if (comando == "search") {
         std::string palavra;
 
         while (true) {
-            std::cout << "Digite q para sair. Palavra buscada: ";
+            std::cout << "Digite . para sair. Palavra buscada: ";
             std::cin >> palavra;
 
-            if (palavra == "q") {
-                break;  // Sai do loop se digitou "q"
+            if (palavra == ".") {
+                break;  // Sai do loop se digitou "."
             }
 
             SearchResult resultado = search(tree, palavra);  
@@ -38,7 +48,7 @@ int main(int argc, char* argv[]) {
                 std::cout << "Tempo de execucao: "<< resultado.executionTime << std::endl;
                 std::cout << "Numero de comparacoes realizadas durante a busca: "<< resultado.numComparisons << std::endl;
             } else {
-                std::cout << palavra << "nao esta presente em nenhum documento." << std::endl;
+                std::cout << palavra << " nao esta presente em nenhum documento." << std::endl;
             }
 
         };
