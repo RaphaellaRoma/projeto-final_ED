b4f677e528c5a2db4d088d5a77ab18d95d2d1a85
Raphaella Roma <raphysics.oficial@gmail.com>
2025-06-02 09:13:06 -0300

    Adicione o arquivo com as funções de leitura dos dados.

--- src/data.cpp
+++ src/data.cpp
@@ -1,1 +1,50 @@
 // Leitura do diretório e carregamento dos arquivos
+
+#include "data.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+vector<string> ler_palavras(const string& caminho) {
+    ifstream arquivo(caminho);
+    vector<string> palavras;
+    string palavra;
+
+    
+    while (arquivo >> palavra) {
+        palavras.push_back(palavra);
+    }
+
+    return palavras;
+}
+
+
+
+vector<doc> read_documents(const string& dir, int n) {
+    vector<doc> documentos;
+
+    for (int i = 1; i <= n; ++i) {
+        string nome_arquivo = dir + "/" + to_string(i) + ".txt";
+        ifstream teste(nome_arquivo);
+        if (!teste.is_open()) {
+            cerr << "Não consegui abrir " << nome_arquivo << ". Pulando.\n";
+            continue;
+        }
+
+        doc doc;
+        doc.id = i; 
+        doc.words = ler_palavras(nome_arquivo);
+        documentos.push_back(doc);
+    }
+
+/*
+    for (const auto& doc : documentos) {
+        cout << "Arquivo " << doc.id << ": " << doc.words.size() << " palavras\n";
+    }
+*/
+    return documentos;
+}
