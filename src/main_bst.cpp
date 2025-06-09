2f7f7574622043ecfbb60634c93c0fcb92c91659
elisa-os <elisasoares2201@gmail.com>
2025-06-09 11:22:43 -0300

    Adicionei o comanto stats

--- src/main_bst.cpp
+++ src/main_bst.cpp
@@ -16,17 +16,17 @@ int main(int argc, char* argv[]) {
 
     BinaryTree* tree = create(); // criar a arvore
 
-    vector<doc> documentos = read_documents(diretorio, n_docs);
-    for (int i = 0; i < n_docs; ++i) {
-        doc document_i = documentos[i];
-        for(size_t p = 0; p < document_i.words.size(); ++p){
-            string word = document_i.words[p];
-            int id = document_i.id;
-            insert(tree, word, id);   // inserir os n_docs documentos do diretorio
-        }
-    }
-
     if (comando == "search") {
+        vector<doc> documentos = read_documents(diretorio, n_docs);
+        for (int i = 0; i < n_docs; ++i) {
+            doc document_i = documentos[i];
+            for(size_t p = 0; p < document_i.words.size(); ++p){
+                string word = document_i.words[p];
+                int id = document_i.id;
+                insert(tree, word, id);   // inserir os n_docs documentos do diretorio
+            }
+        }
+
         std::string palavra;
 
         while (true) {
@@ -53,8 +53,35 @@ int main(int argc, char* argv[]) {
 
         };
     } else if (comando == "stats") {
-        std::cout << "Comando stats ainda nao foi implementado" << std::endl;
-        return 1;
+        // Início da contagem de tempo
+        auto start = std::chrono::high_resolution_clock::now();
+
+        std::vector<doc> documentos = read_documents(diretorio, n_docs);
+
+        // Fim da contagem de tempo
+        auto end = std::chrono::high_resolution_clock::now();
+
+        // Cálculo da duração
+        std::chrono::duration<double, std::milli> duration = end - start;
+        std::cout << "Tempo de leitura: " << duration.count() << " ms" << std::endl;
+
+        int comparisons = 0;
+        double insertiontime = 0.0;
+
+        for (int i = 0; i < n_docs; ++i) {
+            doc document_i = documentos[i];
+            for(size_t p = 0; p < document_i.words.size(); ++p){
+                string word = document_i.words[p];
+                int id = document_i.id;
+                InsertResult insercao_p = insert(tree, word, id);   // inserir os n_docs documentos do diretorio
+                comparisons += insercao_p.numComparisons;
+                insertiontime += insercao_p.executionTime;
+            }
+        }
+        
+        std::cout << "Tempo de insercao: " << insertiontime * 1000 << " ms" << std::endl; // em milisegundos
+        std::cout << "Numero de comparacoes: " << comparisons << std::endl;
+
     } else {
         std::cout << "Comando invalido: " << comando << std::endl;
         std::cout << "Use: search ou stats" << std::endl;
