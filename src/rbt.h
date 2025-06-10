cbaeeace9da2abda49306b8739e94ef7a1f47f12
Raphaella Roma <raphysics.oficial@gmail.com>
2025-06-10 18:02:24 -0300

    Declaração das funções no arquivo header RBT

--- src/rbt.h
+++ src/rbt.h
@@ -10,6 +10,41 @@
 
 namespace RBT{
 
+    /**
+    * @brief Cria e inicializa uma árvore binária vazia.
+    * 
+    * @return BinaryTree* Ponteiro para a nova árvore criada. Retorna nullptr em caso de falha na alocação de memória.
+    */
+    BinaryTree* create();
+
+
+    /**
+    * @brief Corrige a árvore para manter as propriedades da RBT.
+    * 
+    * Se a palavra já existir na árvore, adiciona o ID do documento na lista de IDs, caso ainda não esteja presente.
+    * Mede o tempo de execução da operação e conta o número de comparações realizadas durante a inserção.
+    * 
+    * @param tree Ponteiro para a árvore binária onde a palavra será inserida.
+    * @param node Nó inserido.
+    */ 
+    void fixUp(BinaryTree* tree, Node* node);
+
+
+    /**
+    * @brief Insere uma palavra na árvore binária, associando-a a um ID de documento.
+    * 
+    * Se a palavra já existir na árvore, adiciona o ID do documento na lista de IDs, caso ainda não esteja presente.
+    * Mede o tempo de execução da operação e conta o número de comparações realizadas durante a inserção.
+    * 
+    * @param tree Ponteiro para a árvore binária onde a palavra será inserida.
+    * @param word Palavra a ser inserida.
+    * @param documentId ID do documento associado à palavra.
+    * @return InsertResult Estrutura contendo informações sobre a inserção:
+    *         - numComparisons: número de comparações realizadas;
+    *         - executionTime: tempo gasto na operação, em segundos.
+    */ 
+    InsertResult insert(BinaryTree* tree, const std::string& word, int documentId);
+
 }
 
 #endif
