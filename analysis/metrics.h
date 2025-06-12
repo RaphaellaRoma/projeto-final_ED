8ca5205fdce08332b08954ed95bb43931bc1ca4c
SamyraMara <samyrinhamara@gmail.com>
2025-06-12 01:15:12 -0300

    criando funções ultilitarias para analise

--- analysis/metrics.h
+++ analysis/metrics.h
@@ -1,1 +1,29 @@
-//Funções utilitárias para medir tempo, comparações, altura, etc.
+//Funções utilitárias de coleta e salvamento de métricas para medir tempo, comparações, altura, etc.
+#ifndef METRICS_H
+#define METRICS_H
+
+#include <string>
+
+struct Metrics {
+    std::string estrutura;
+    int qtdDocumentos;
+    double tempoTotalInsercao;
+    double tempoMedioInsercao;
+    double tempoTotalBusca;
+    double tempoMedioBusca;
+    int altura;
+    int menorGalho;
+    int maiorGalho;
+    int totalComparacoesInsercao;
+    int totalComparacoesBusca;
+};
+
+// Funções livres:
+std::string gerarCabecalhoCSV();
+std::string gerarLinhaCSV(const Metrics& m);
+
+#endif
+
+
+
+
