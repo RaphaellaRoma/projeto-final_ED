e348192606c65a18582c14ccecf3a1024f6699f2
SamyraMara <samyrinhamara@gmail.com>
2025-06-17 20:11:09 -0300

    Update Analises - crie mais uma metrica

--- analysis/metrics.cpp
+++ analysis/metrics.cpp
@@ -5,7 +5,8 @@ Metrics iniciarMetrics(const std::string& estrutura) {
     Metrics m;
     m.estrutura = estrutura;
     m.qtdDocumentos = 0;
-    m.qtdPalavras = 0;
+    m.qtdPalavrasTotais = 0;
+    m.qtdPalavrasDist = 0;
     m.tempoTotalInsercao = 0.0;
     m.tempoMedioInsercao = 0.0;
     m.tempoTotalBusca = 0.0;
@@ -20,13 +21,14 @@ Metrics iniciarMetrics(const std::string& estrutura) {
 }
 
 std::string gerarCabecalhoCSV() {
-    return "Estrutura,Documentos,NumeroPalavras,TempoTotalInsercao,TempoMedioInsercao,TempoTotalBusca,TempoMedioBusca,Altura,MenorGalho,MaiorGalho,ComparacoesInsercao,ComparacoesBusca,NumeroRotacao\n";
+    return "Estrutura,Documentos,NumeroPalavrasTotais,NumeroPalavrasDistintas,TempoTotalInsercao,TempoMedioInsercao,TempoTotalBusca,TempoMedioBusca,Altura,MenorGalho,MaiorGalho,ComparacoesInsercao,ComparacoesBusca,NumeroRotacao\n";
 }
 
 std::string gerarLinhaCSV(const Metrics& m) {
     return m.estrutura + "," +
            std::to_string(m.qtdDocumentos) + "," +
-            std::to_string(m.qtdPalavras) + "," +
+           std::to_string(m.qtdPalavrasTotais) + "," +
+           std::to_string(m.qtdPalavrasDist) + "," +
            std::to_string(m.tempoTotalInsercao) + "," +
            std::to_string(m.tempoMedioInsercao) + "," +
            std::to_string(m.tempoTotalBusca) + "," +
